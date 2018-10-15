f2770ec8b5de3837604893df98679526eae33bb8
Lucio Paiva <luciopaiva@gmail.com>
2018-10-15 20:58:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+
+# Canvas CPU benchmark
+
+How fast can we write to a canvas, pixel by pixel, using solely the CPU (i.e., no WebGL)?
+
+This benchmark focus on writing to a buffer and handing it to the canvas context. Some variations are attempted and measured to understand how to efficiently paint the canvas for tasks that require pixel manipulation.
+
+## Writing four bytes at once
+
+Instead of writing every pixel one channel at a time:
+
+    for (let i = 0; i < canvasSizeInBytes; i += 4) {
+        buffer[i] = 0;
+        buffer[i+1] = 255;
+        buffer[i+2] = 0;
+        buffer[i+3] = 255;
+    }
+
+It is more efficient to write once per pixel using `Uint32Array`s:
+
+    const view = new Uint32Array(buffer.buffer);
+
+    for (let i = 0; i < canvasSizeInPixels; i++) {
+        view[i] = (255 << 24) & (255 << 8);
+    }
+
+This has a huge impact on performance, since gains scale with the number of pixels.
+
+## Avoid calling getImageData()
+
+This is a minor, since it'll only happen once per frame, but anyway. Instead of getting a new buffer for every new frame draw:
+
+    const imageData = ctx.getImageData(0, 0, width, height);
+    const buffer = imageData.data;
+
+The best way is to create a single buffer once and reuse it during execution. Just move it outside the frame rendering function - it will work just the same.
+
+## Some results
+
+System:
+
+- OS: `Intel® Core™ i5-4670 CPU @ 3.40GHz × 4`
+- CPU: `Intel® Core™ i5-4670 CPU @ 3.40GHz × 4`
+
+Each test ran for 2 minutes. Results:
+
+- canvas size: `500 x 500`
+- Uint8ClampedArray: 7.4ns per pixel
+- Uint32Array: 4.7ns per pixel
+
+So the speedup was of 1.57 times.
+
+But time per pixel curiously decreases when the canvas gets bigger:
+
+- canvas size: `1000 x 1000`
+- Uint8ClampedArray: 4.5ns per pixel
+- Uint32Array: 2.9ns per pixel
+
+Speedup still similar, though, 1.55 times.
---
+++ index.js
@@ -0,0 +1,69 @@
+
+function readCssVar(varName) {
+    varName = varName.startsWith("--") ? varName : "--" + varName;
+    return getComputedStyle(document.documentElement).getPropertyValue(varName);
+}
+
+function readCssVarAsNumber(varName) {
+    return parseInt(readCssVar(varName), 10);
+}
+
+// misc constants
+const width = readCssVarAsNumber("width");
+const height = readCssVarAsNumber("height");
+const canvasSizeInPixels = width * height;
+const fullHdSizeInPixels = 1920 * 1080;
+
+// canvas objects
+const canvas = document.getElementById("canvas");
+canvas.setAttribute("width", width);
+canvas.setAttribute("height", height);
+const ctx = canvas.getContext("2d");
+const imageData = ctx.getImageData(0, 0, width, height);
+const buffer = imageData.data;
+const view = new Uint32Array(buffer.buffer);
+
+// metrics
+const fpsElem = document.getElementById("fps");
+const timePerPixelElem = document.getElementById("time-per-pixel");
+const avgTimePerPixelElem = document.getElementById("avg-time-per-pixel");
+const fullHdTimeElem = document.getElementById("full-hd-time");
+let elapsedSum = 0;
+let elapsedCount = 0;
+let accumulatedTimePerPixelSum = 0;
+let accumulatedTimePerPixelCount = 0;
+
+function reportMetrics() {
+    const elapsed = elapsedSum / elapsedCount;
+    const elapsedPerPixelInNanos = 1000000 * elapsed / canvasSizeInPixels;
+
+    accumulatedTimePerPixelSum += elapsedPerPixelInNanos;
+    accumulatedTimePerPixelCount++;
+    const accumulatedTimePerPixel = accumulatedTimePerPixelSum / accumulatedTimePerPixelCount;
+
+    fpsElem.innerText = elapsedCount;
+    timePerPixelElem.innerText = elapsedPerPixelInNanos.toFixed(1);
+    avgTimePerPixelElem.innerText = accumulatedTimePerPixel.toFixed(1);
+    fullHdTimeElem.innerText = (elapsedPerPixelInNanos * fullHdSizeInPixels / 1000000).toFixed(1);
+    elapsedSum = elapsedCount = 0;
+}
+
+function update() {
+    // I don't want to trust the time informed as param to update(), since it seems to be measured way before this
+    // function actually getting called
+    const startTime = performance.now();
+
+    for (let i = 0; i < canvasSizeInPixels; i++) {
+        view[i] = 0xff000000;
+    }
+
+    ctx.putImageData(imageData, 0, 0, 0, 0, width, height);
+
+    elapsedSum += performance.now() - startTime;
+    elapsedCount++;
+
+    requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
+setInterval(reportMetrics, 1000);
